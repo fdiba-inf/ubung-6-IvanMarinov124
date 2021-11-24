fde2eccc27d430ea59b73379b9638758aff4aa43
IvanMarinov124 <>
2021-11-24 21:01:38 +0000

    Files ready

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+
+        int numberReverse = 0;
+        int mNum = n;
+
+        while (mNum > 0)
+        {
+          int nextDigit = mNum % 10;
+          numberReverse = numberReverse * 10 + nextDigit;
+          mNum = mNum / 10;
+        }
+        boolean palindrom = n == numberReverse;
+        System.out.println("Palindrome: " + palindrom);
+
     }
 }
+
